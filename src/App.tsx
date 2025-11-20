f95a9cc7ac4be7d1c0aaf4b3a53bd489bd546aa6
LeeDongJun <djleee1995@pointi.com>
2025-11-20 11:13:34 +0900

    헤더수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { useEffect } from "react";
 import ApiTest from "./pages/ApiTest";
 
 export default function App() {
+  useEffect(() => {
+    document.title = import.meta.env.VITE_PROJECT_NAME;
+  }, []);
+
   return <ApiTest />;
 }
